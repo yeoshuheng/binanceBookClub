39ae72b9727a248218de8ebeb30f705ee92cb223
yeoshuheng <100367948+yeoshuheng@users.noreply.github.com>
2025-07-03 17:05:32 +0800

    feat: add object pools

--- src/commons/LockFreePool.cpp
+++ src/commons/LockFreePool.cpp
@@ -3,3 +3,39 @@
 //
 
 #include "LockFreePool.h"
+
+template<typename T, size_t init_size, size_t capacity>
+LockFreePool<T, init_size, capacity>::LockFreePool() {
+    for (size_t i = 0; i < init_size; i++) {
+        T* obj = new T();
+        object_pool.push_back(obj);
+    }
+}
+
+template<typename T, size_t init_size, size_t capacity>
+LockFreePool<T, init_size, capacity>::~LockFreePool() {
+    T* obj;
+    while (object_pool.pop(obj)) {
+        delete obj;
+    }
+}
+
+template<typename T, size_t init_size, size_t capacity>
+std::unique_ptr<T, std::function<void(T*)>> LockFreePool<T, init_size, capacity>::borrow_object() {
+    T* obj = nullptr;
+    if (!object_pool.pop()) {
+        obj = new T();
+    }
+    return std::unique_ptr<T, std::function<void(T*)>>(obj, [this](T* obj_ptr) {
+        this->return_object(obj_ptr);
+    });
+}
+
+template<typename T, size_t init_size, size_t capacity>
+void  LockFreePool<T, init_size, capacity>::return_object(T* obj_ptr) {
+    if (!object_pool.push(obj_ptr)) {
+        delete obj_ptr;
+    }
+}
+
+
--- src/commons/LockFreePool.h
+++ src/commons/LockFreePool.h
@@ -4,13 +4,23 @@
 
 #ifndef LOCKFREEPOOL_H
 #define LOCKFREEPOOL_H
+#include <functional>
+#include <memory>
+#include <boost/lockfree/stack.hpp>
 
-
-
+template<typename T, size_t init_size, size_t capacity>
 class LockFreePool {
+    boost::lockfree::stack<T*> object_pool{capacity};
+    void return_object(T* obj_ptr);
+public:
+    LockFreePool();
+    ~LockFreePool();
 
+    /**
+     * Borrows a object from the pool.
+     * @return Unique pointer to the object that has a custom deleter to return the object to the pool once out of scope.
+     */
+    std::unique_ptr<T, std::function<void(T*)>> borrow_object();
 };
-
-
 
 #endif //LOCKFREEPOOL_H
